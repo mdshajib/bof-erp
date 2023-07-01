ad333b6722e04f5623513cce46f9e7d8df73308f
manjurulhasan <manjurul.hasan@selise.ch>
2023-07-01 21:50:29 +0600

    sidebar bf color changed

--- resources/views/components/common/aside.blade.php
+++ resources/views/components/common/aside.blade.php
@@ -9,20 +9,21 @@
                 <li class="menu-title" data-key="t-menu">Menu</li>
                 <x-menu.item text="{{ __('Dashboard') }}" link="{{ route('dashboard') }}" icon="home" class="{{ (request()->is('dashboard*')) ? 'active' : '' }} "/>
                 <x-menu.item text="{{ __('Manage Users') }}" link="{{ route('user.manage') }}" icon="users" class="{{ (request()->is('users')) ? 'active' : '' }}"/>
+
                 <x-menu.list text="Category Management" icon="list" class="{{ (request()->is('categor*')) ? 'mm-active' : '' }}">
                     <x-menu.item text="Manage Category" link="{{  route('manage.category') }}" class="{{ (request()->is('categories')) ? 'active' : '' }}"/>
                     <x-menu.item text="Manage Attribute" link="{{  route('manage.category') }}" class="{{ (request()->is('categories')) ? 'active' : '' }}"/>
                     <x-menu.item text="Manage Terms" link="{{  route('manage.category') }}" class="{{ (request()->is('categories')) ? 'active' : '' }}"/>
                 </x-menu.list>
 
-                <x-menu.item text="Supplier Management" link="{{ route('supplier.manage') }}" icon="home" class="{{ (request()->is('suppliers*')) ? 'active' : '' }} "/>
+                <x-menu.item text="Supplier Management" link="{{ route('supplier.manage') }}" icon="cpu" class="{{ (request()->is('suppliers*')) ? 'active' : '' }} "/>
 
                 <x-menu.list text="Product Management" icon="package" class="{{ (request()->is('product*')) ? 'mm-active' : '' }}">
                     <x-menu.item text="Add Product" link="{{ route('product.create') }}" class="{{ (request()->is('products/create')) ? 'active' : '' }}"/>
                     <x-menu.item text="Manage Products" link="{{ route('product.manage') }}" class="{{ (request()->is('products')) ? 'active' : '' }} "/>
                 </x-menu.list>
 
-                <x-menu.list text="Purchase Management" icon="shopping-cart" class="{{ (request()->is('purchases*')) ? 'mm-active' : '' }}">
+                <x-menu.list text="Purchase Management" icon="shopping-bag" class="{{ (request()->is('purchases*')) ? 'mm-active' : '' }}">
                     <x-menu.item text="Purchase Order" link="{{ route('purchase.create') }}" class="{{ (request()->is('purchases/create')) ? 'active' : '' }}"/>
                     <x-menu.item text="All Purchase" link="{{ route('purchase.manage') }}" class="{{ (request()->is('purchases')) ? 'active' : '' }}"/>
                 </x-menu.list>
