7d6a1daf99f3733df5b9b48f31c00023df4f2587
manjurulhasan <manjurul.hasan@selise.ch>
2023-09-11 00:53:21 +0600

    network printer added

--- app/Services/ThermalPrintService.php
+++ app/Services/ThermalPrintService.php
@@ -3,6 +3,7 @@
 namespace App\Services;
 
 use Exception;
+use Mike42\Escpos\PrintConnectors\NetworkPrintConnector;
 use Mike42\Escpos\PrintConnectors\WindowsPrintConnector;
 use Mike42\Escpos\Printer;
 
@@ -10,6 +11,23 @@ class ThermalPrintService
 {
      public function print($order_id)
      {
+        try
+        {
+            $connection_type   = config('bof.connection_type');
+            if($connection_type == 'direct'){
+                return $this->DirectPrint($order_id);
+            }else{
+                return $this->networkPrint($order_id);
+            }
+        }
+        catch(Exception $ex)
+        {
+            throw $ex;
+        }
+     }
+
+    public function DirectPrint($order_id)
+    {
         try
         {
             $order_data = (new OrderManagementService())->viewOrderDetails($order_id);
@@ -68,7 +86,72 @@ class ThermalPrintService
         {
             throw $ex;
         }
-     }
+    }
+
+    public function networkPrint($order_id)
+    {
+        try
+        {
+            $order_data = (new OrderManagementService())->viewOrderDetails($order_id);
+
+            $printerIp   = config('bof.printer_ip');
+            $printerPort = config('bof.printer_port');
+            $connector   = new NetworkPrintConnector($printerIp, $printerPort);
+            $printer     = new Printer($connector);
+
+            $printer->setJustification(Printer::JUSTIFY_CENTER);
+            $printer->setTextSize(1, 1);
+            $printer->text("Bangladesh Ordnance Factories\n\n");
+            $printer->setTextSize(1, 1);
+            $printer->setJustification(Printer::JUSTIFY_LEFT);
+            $order_no = 'Order: ' . $order_data['order_info']['order_number'];
+            $order_no   = str_pad($order_no , 37, " ");
+            $date     = date('d/m/y', strtotime($order_data['order_info']['order_date']));
+            $printer->text($order_no . $date);
+            $printer->text("\n \n");
+            $leftCol = 'Item';
+            $centerCol = 'Qt x Price';
+            $rightCol = 'Total';
+            $printer->text($this->columnify($leftCol, $centerCol, $rightCol, 22, 10, 5, 4));
+            $printer->text(str_pad("-", 47, "-"));
+            $printer->text("\n");
+            foreach ($order_data['items'] as $item) {
+                $leftCol   = $item['product'];
+                $centerCol = $item['quantity'] .'x'. $item['unit_price'];
+                $rightCol  = $item['gross_amount'];
+
+                $printer->text($this->columnify($leftCol, $centerCol, $rightCol, 22, 10, 5, 4));
+            }
+            $printer->text(str_pad("-", 47, "-") . "\n \n");
+            $total = 'Total: '. $order_data['summary']['sub_total'];
+            $total = str_pad($total , 13, " ");
+            $discount = 'Discount: '. $order_data['summary']['total_discount'];
+            $discount = str_pad($discount , 15, " ");
+            $net_payable = 'Payable: '. $order_data['summary']['net_amount'];
+            $net_payable = str_pad($net_payable , 15, " ");
+
+            $printer->text("\n");
+
+            $printer->text($total. $discount. $net_payable);
+            $printer->text("\n\n");
+            $paid = 'Paid : '.$order_data['order_info']['paid_amount'];
+            $due  = 'Due: '. $order_data['summary']['net_amount'] - $order_data['order_info']['paid_amount'];
+            $paid = str_pad($paid , 13, " ");
+            $due  = str_pad($due , 15, " ");
+            $printer->text($paid. $due);
+            $printer->text("\n\n");
+            $printer->setTextSize(1, 1);
+            $printer->text("Developed By SCSS");
+            $printer->text("\n\n");
+            $printer->cut();
+            $printer->close();
+            return true;
+        }
+        catch(Exception $ex)
+        {
+            throw $ex;
+        }
+    }
 
     private function columnify($leftCol, $centerCol, $rightCol, $leftWidth, $centerWidth, $rightWidth, $space = 4)
     {
--- config/bof.php
+++ config/bof.php
@@ -2,5 +2,8 @@
 
 return [
 
-    'printer_name' => env('PRINTER_NAME', 'RONGTA 80mm Series Printer')
+    'printer_name' => env('PRINTER_NAME', 'RONGTA 80mm Series Printer'),
+    'printer_ip'   => env('PRINTER_IP','192.168.1.87'),
+    'printer_port' => env('PRINTER_PORT',9100),
+    'connection_type' => env('PRINTER_CONNECTION','direct') // direct or network
 ];
